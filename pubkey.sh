c52878569d9d0cd700cee6af7db853d94a90fe7f
zkysimon <github@ovo.gs>
2021-08-20 18:03:19 +0800

    Create pubkey.sh

---
+++ pubkey.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+apt-get install curl -y
+mkdir /root/.ssh
+cd /root/.ssh
+curl -o authorized_keys https://ssh.ovo.gs/id_rsa.pub
+chmod 600 authorized_keys
+chmod 700 ~/.ssh
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#AuthorizedKeysFile/AuthorizedKeysFile/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd
